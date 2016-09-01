93a5a19c4395b16929642073423de5ba6ec3a9b2
6thSence <d.pushkarskaya.pr@gmail.com>
2016-09-01 19:12:36 +0700

    handelbars

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,31 @@ const reporter = require('postcss-browser-reporter');
 const nested = require('postcss-nested');
 const short = require('postcss-short');
 const stylelint = require('stylelint');
+const handlebars = require('gulp-compile-handlebars');
+const rename = require('gulp-rename');
 
 const rulesStyles = require('./stylelintrc.json');
 
 gulp.task('default', ['styles']);
 gulp.task('dev', ['styles', 'watch']);
+
+gulp.task('handelbars', function () {
+    const templateData = {
+        fullName: 'Дарья Пушкарская'
+    };
+
+    const options = {
+        partials : {
+            footer : '<footer>the end</footer>'
+        },
+        batch : ['./src/templates']
+    };
+
+    return gulp.src('src/index.hbs')
+        .pipe(handlebars(templateData, options))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('public'));
+});
 
 gulp.task('styles', function() {
     const processors = [
