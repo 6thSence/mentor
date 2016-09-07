a75a1403a75b73cb3bc3fea883996d2eb4b74ee7
6thSence <d.pushkarskaya.pr@gmail.com>
2016-09-08 00:35:51 +0700

    come back dirname

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
+app.get('/', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
